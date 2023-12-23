3d45f0bdd85c9fba9e6162606218712df1c1a290
Ayush Kumar Sharma <64814485+Ayush5935@users.noreply.github.com>
2023-12-23 11:51:39 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,23 @@
+// backend/app.js
+const express = require('express');
+const app = express();
+const PORT = 3001;
+
+// Simulating the switch to a new backend
+let useNewBackend = false;
+
+app.get('/api/data', (req, res) => {
+  const responseData = useNewBackend ? 'New backend data' : 'Old backend data';
+  res.json({ message: responseData });
+});
+
+// Endpoint to toggle between backends
+app.get('/api/switch', (req, res) => {
+  useNewBackend = !useNewBackend;
+  res.json({ message: `Switched to ${useNewBackend ? 'new' : 'old'} backend` });
+});
+
+app.listen(PORT, () => {
+  console.log(`Backend listening on port ${PORT}`);
+});
+￼Enter
