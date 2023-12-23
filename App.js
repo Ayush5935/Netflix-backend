5180e90c79cfdab926452c9bd9d0ed9deb8c5c25
Ayush Kumar Sharma <64814485+Ayush5935@users.noreply.github.com>
2023-12-23 13:25:05 +0530

    Update App.js

--- App.js
+++ App.js
@@ -3,6 +3,14 @@ const express = require('express');
 const app = express();
 const PORT = 3001;
 
+// Middleware to set headers to prevent caching
+app.use((req, res, next) => {
+  res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
+  res.header('Expires', '-1');
+  res.header('Pragma', 'no-cache');
+  next();
+});
+
 // Simulating the switch to a new backend
 let useNewBackend = false;
 
